90afdce74283ba12991e5aeb73a798e1cefb6bde
Uzmah Shaikh <adibashaikh1506@gmail.com>
2024-01-23 10:03:48 +0530

    First Commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor{
+  static Color primaryColor = Color(0xFFFA9884);
+  static Color secondaryColor = Color(0xFFE74646);
+  static Color accentColor = Color(0xFFFFE5CA);
+}
